d836db9ade28f4db4cae317a4256b35f121689a8
Congwen Ma <lostaznfound@gmail.com>
2018-02-10 16:34:20 -0600

    fix pawn friendly fire

--- src/elm/Models/Pawn.elm
+++ src/elm/Models/Pawn.elm
@@ -3,6 +3,7 @@ module Models.Pawn exposing (..)
 import Models.Coordinate exposing (..)
 import Models.Piece exposing (Piece)
 import Models.Avatar exposing (FactionType(..))
+import ChessUtils exposing (areAnyPieceOnCoordinate, areAnyEnemiesOnCoordinate)
 
 
 -- TODO: promotion
@@ -16,40 +17,47 @@ getMovePotential { coordinate, avatar } allPieces =
 
         { faction } =
             avatar
+
+        moves =
+            case faction of
+                BlackPlayer ->
+                    let
+                        oneStep =
+                            Coordinate x (y + 1)
+
+                        twoStep =
+                            Coordinate x (y + 2)
+
+                        noPieceOnOneStep =
+                            List.all (\piece -> piece.coordinate /= oneStep) allPieces
+                    in
+                        if y == 1 && noPieceOnOneStep then
+                            [ oneStep, twoStep ]
+                        else
+                            [ oneStep ]
+
+                WhitePlayer ->
+                    let
+                        oneStep =
+                            Coordinate x (y - 1)
+
+                        twoStep =
+                            Coordinate x (y - 2)
+
+                        noPieceOnOneStep =
+                            List.all (\piece -> piece.coordinate /= oneStep) allPieces
+                    in
+                        if y == 6 && noPieceOnOneStep then
+                            -- and black
+                            [ oneStep, twoStep ]
+                        else
+                            [ oneStep ]
     in
-        case faction of
-            BlackPlayer ->
-                let
-                    oneStep =
-                        Coordinate x (y + 1)
-
-                    twoStep =
-                        Coordinate x (y + 2)
-
-                    noPieceOnOneStep =
-                        List.all (\piece -> piece.coordinate /= oneStep) allPieces
-                in
-                    if y == 1 && noPieceOnOneStep then
-                        [ oneStep, twoStep ]
-                    else
-                        [ oneStep ]
-
-            WhitePlayer ->
-                let
-                    oneStep =
-                        Coordinate x (y - 1)
-
-                    twoStep =
-                        Coordinate x (y - 2)
-
-                    noPieceOnOneStep =
-                        List.all (\piece -> piece.coordinate /= oneStep) allPieces
-                in
-                    if y == 6 && noPieceOnOneStep then
-                        -- and black
-                        [ oneStep, twoStep ]
-                    else
-                        [ oneStep ]
+        List.filter
+            (\coord ->
+                not <| areAnyPieceOnCoordinate allPieces coord
+            )
+            moves
 
 
 getKillPotential : Piece -> List Piece -> List Coordinate
@@ -60,10 +68,17 @@ getKillPotential { coordinate, avatar } allPieces =
 
         { faction } =
             avatar
-    in
-        case faction of
-            BlackPlayer ->
-                [ Coordinate (x + 1) (y + 1), Coordinate (x - 1) (y + 1) ]
 
-            WhitePlayer ->
-                [ Coordinate (x + 1) (y - 1), Coordinate (x - 1) (y - 1) ]
+        moves =
+            case faction of
+                BlackPlayer ->
+                    [ Coordinate (x + 1) (y + 1), Coordinate (x - 1) (y + 1) ]
+
+                WhitePlayer ->
+                    [ Coordinate (x + 1) (y - 1), Coordinate (x - 1) (y - 1) ]
+    in
+        List.filter
+            (\coord ->
+                areAnyEnemiesOnCoordinate avatar allPieces coord
+            )
+            moves
